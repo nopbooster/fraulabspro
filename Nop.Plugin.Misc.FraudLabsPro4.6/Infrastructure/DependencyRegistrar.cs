bf15120c20a341d5360582ec85230d2fe2eb47d9
ayushman <ayuceman@gmail.com>
2024-03-05 12:29:47 +0545

    Add Fraudlabs project

---
+++ Nop.Plugin.Misc.FraudLabsPro4.6/Infrastructure/DependencyRegistrar.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nop.Core.Configuration;
+using Nop.Core.Infrastructure;
+using Nop.Plugin.Misc.FraudLabsPro.Factories;
+using Nop.Plugin.Misc.FraudLabsPro.Services;
+
+namespace Nop.Plugin.Misc.FraudLabsPro.Infrastructure
+{
+
+    /// <summary>
+    /// Represents a FraudLabs Pro dependency registrar
+    /// </summary>
+    /// 
+
+    public class DependencyRegistrar : INopStartup
+    {
+        public void Configure(IApplicationBuilder application)
+        {
+
+        }
+
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            //register custom services
+            services.AddScoped<FraudLabsProManager>();
+
+            //register custom factories
+            services.AddScoped<FraudLabsProOrderModelFactory>();
+        }
+
+        public int Order => 2;
+    }
+
+}
---
+++ Nop.Plugin.Misc.FraudLabsPro4.6/Services/EventConsumer.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading.Tasks;
+using Nop.Core.Domain.Orders;
+using Nop.Services.Cms;
+using Nop.Services.Events;
+using Nop.Web.Framework.Events;
+using Nop.Web.Framework.UI;
+
+namespace Nop.Plugin.Misc.FraudLabsPro.Services
+{
+    /// <summary>
+    /// Represents a FraudLabsPro event consumer
+    /// </summary>
+    public class EventConsumer :
+        IConsumer<OrderPlacedEvent>,
+        IConsumer<PageRenderingEvent>
+    {
+        #region Fields
+
+        private readonly FraudLabsProManager _fraudLabsProManager;
+        private readonly IWidgetPluginManager _widgetPluginManager;
+
+        #endregion
+
+        #region Ctor
+
+        public EventConsumer(
+            FraudLabsProManager fraudLabsProManager,
+            IWidgetPluginManager widgetPluginManager
+            )
+        {
+            _fraudLabsProManager = fraudLabsProManager;
+            _widgetPluginManager = widgetPluginManager;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Handle the order placed event
+        /// </summary>
+        /// <param name="eventMessage">The event message.</param>
+        public async Task HandleEventAsync(OrderPlacedEvent eventMessage)
+        {
+            //check whether the plugin is active
+            if (!await _widgetPluginManager.IsPluginActiveAsync(FraudLabsProDefaults.SystemName))
+                return;
+
+            //handle event
+            await _fraudLabsProManager.HandleOrderPlacedEventAsync(eventMessage.Order);
+        }
+
+        /// <summary>
+        /// Handle page rendering event
+        /// </summary>
+        /// <param name="eventMessage">Event message</param>
+        public async Task HandleEventAsync(PageRenderingEvent eventMessage)
+        {
+            //check whether the plugin is active
+            if (!await _widgetPluginManager.IsPluginActiveAsync(FraudLabsProDefaults.SystemName))
+                return;
+
+            //add js sсript to the one page checkout
+            var routeName = eventMessage.GetRouteName() ?? string.Empty;
+            if (routeName == FraudLabsProDefaults.OnePageCheckoutRouteName || routeName == FraudLabsProDefaults.ConfirmCheckoutRouteName)
+            {
+                eventMessage.Helper.AddScriptParts(ResourceLocation.Footer, FraudLabsProDefaults.AgentScriptPath, excludeFromBundle: true);
+            }
+        }
+
+        #endregion
+    }
+}
